6c7a85eb2fdeed4115e9c52d43b337c48cce6d06
VinceZCL <vincewcode@gmail.com>
2025-08-20 19:27:56 +0800

    added cors configuration, data call successful

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { inject } from '@angular/core';
+
+import { User } from './models/user';
+import { UserService } from './services/user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  users: User[] = [];
+
+  private userService = inject(UserService);
+
+  ngOnInit() : void {
+    this.loadUsers();
+  }
+
+  loadUsers() : void {
+    this.userService.getUsers().subscribe({
+      next: (event: User[]) => {
+        console.log(event);
+        this.users = event;
+      },
+      error: (error: Error) => {
+        console.log(error);
+      }
+    })
+  }
 }
